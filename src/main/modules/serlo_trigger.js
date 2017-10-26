03c944609df2fbe3fddf24d87cc36b6fe425c044
Jonas Keinholz <jonas@keinholz.com>
2017-10-26 17:59:29 +0200

    chore: import assets from athene2

---
+++ src/main/modules/serlo_trigger.js
@@ -0,0 +1,23 @@
+import $ from 'jquery'
+
+var TriggerAction
+
+TriggerAction = function () {
+  return $(this).each(function () {
+    // Edit mode toggle
+    if ($(this).data('trigger') === 'ping') {
+      $(this).click(function () {
+        var $that = $(this)
+        var location = $that.data('href')
+
+        if (location) {
+          $.ajax({
+            url: location
+          })
+        }
+      })
+    }
+  })
+}
+
+$.fn.TriggerAction = TriggerAction
