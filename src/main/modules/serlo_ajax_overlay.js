fef5fa6afb61302d458fe45a4da1503d85915544
Jonas Keinholz <jonas@keinholz.com>
2018-11-18 09:59:37 +0100

    chore: license headers

--- src/main/modules/serlo_ajax_overlay.js
+++ src/main/modules/serlo_ajax_overlay.js
@@ -1,16 +1,23 @@
 /**
- * $.SerloAjaxOverlay opens links in an overlay.
- * Creates an AjaxPage for every asynchronous loaded content,
- * caches them and provides tabs
+ * This file is part of Athene2 Assets.
  *
- * Available Callbacks:
- * contentLoaded, contentOpened, beforeClose, beforeOpen, afterClose, beforeClose
+ * Copyright (c) 2017-2018 Serlo Education e.V.
  *
- * Usage: $.SerloAjaxOverlay({on: {contentLoaded: function () { ... }}});
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
  *
- * @class AjaxOverlay
- * @constructor
- * @param {Object} options Check 'defaults' in code
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2013-2018 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2-assets for the canonical source repository
  */
 import $ from 'jquery'
 
