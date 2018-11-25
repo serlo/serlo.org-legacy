483349f28013f33c3088de1552f6c59dc63eed3b
Jonas Keinholz <jonas@keinholz.com>
2018-11-25 12:32:24 +0100

    feat: configure Sentry as runtime error monitoring

---
+++ src/main/modules/serlo_sentry.js
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/browser'
+
+import { version } from '../../../package.json'
+
+Sentry.init({
+  dsn:
+    process.env.NODE_ENV === 'development'
+      ? 'https://0c66c811e7f4408c8f20798379d7a814@sentry.io/1330033'
+      : null,
+  release: `athene2-assets@${version}`
+})
+
+export { Sentry }
