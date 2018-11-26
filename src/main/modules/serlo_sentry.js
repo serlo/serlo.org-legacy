9bf0b554162ddab3a9c9bb730cddd24954d8ff09
Jonas Keinholz <jonas@keinholz.com>
2018-11-26 14:21:00 +0100

    fix: enable Sentry in production mode

--- src/main/modules/serlo_sentry.js
+++ src/main/modules/serlo_sentry.js
@@ -4,7 +4,7 @@ import { version } from '../../../package.json'
 
 Sentry.init({
   dsn:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === 'production'
       ? 'https://0c66c811e7f4408c8f20798379d7a814@sentry.io/1330033'
       : null,
   release: `athene2-assets@${version}`
