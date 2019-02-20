3b7808100bc906bfdd4f561761b5e47293752135
Jonas Keinholz <jonas@keinholz.com>
2019-02-20 06:11:12 +0100

    feat(gcloud): extract package

---
+++ packages/gcloud/index.ts
@@ -0,0 +1,35 @@
+import { spawnSync } from 'child_process'
+import * as path from 'path'
+
+export function auth() {
+  spawnSync('gcloud', ['init'])
+  spawnSync('gcloud', [
+    'auth',
+    'activate-service-account',
+    `--key-file=${path.join(__dirname, 'gcloud.secret.json')}`
+  ])
+}
+
+export function uploadFolder({
+  bucket,
+  source,
+  target
+}: {
+  bucket: string
+  source: string
+  target: string
+}) {
+  const b = `gs://${bucket}`
+  const dest = `${b}/${trimSlashes(target)}/`
+  const tmp = `${b}/${trimSlashes(target)}-tmp/`
+
+  spawnSync(`gsutil`, ['-m', 'cp', '-r', path.join(source, '*'), tmp], {
+    stdio: 'inherit'
+  })
+  spawnSync(`gsutil`, ['-m', 'rm', '-r', dest], { stdio: 'inherit' })
+  spawnSync(`gsutil`, ['-m', 'mv', `${tmp}*`, dest], { stdio: 'inherit' })
+
+  function trimSlashes(p: string) {
+    return p.replace(/^\/+/, '').replace('//+$/', '')
+  }
+}
