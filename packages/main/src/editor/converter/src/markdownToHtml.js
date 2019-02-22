359bb91a5648e68f3eb493bb94d6650ed78cc147
Jonas Keinholz <jonas@keinholz.com>
2019-02-22 14:08:56 +0100

    fix: unintended WebStorm automation

--- packages/main/src/editor/converter/src/markdownToHtml.js
+++ packages/main/src/editor/converter/src/markdownToHtml.js
@@ -19,7 +19,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/athene2-assets for the canonical source repository
  */
-import { converter } from '../../../../../markdown/src'
+import { converter } from '@serlo/markdown'
 
 const renderMarkdown = input => {
   let html = converter.makeHtml(input)
