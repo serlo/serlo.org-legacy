180bb9d57f05b3f712d3881d54b38ee126ace571
Jonas Keinholz <git@inyono.dev>
2019-06-20 06:32:58 +0200

    chore(main/sentry): only accept issues from serlo domains

--- packages/main/src/main/modules/sentry.js
+++ packages/main/src/main/modules/sentry.js
@@ -28,7 +28,8 @@ Sentry.init({
     process.env.NODE_ENV === 'production'
       ? 'https://0c66c811e7f4408c8f20798379d7a814@sentry.io/1330033'
       : null,
-  release: `athene2-assets@${version}`
+  release: `athene2-assets@${version}`,
+  whitelistUrls: ['serlo.org', 'serlo-development.dev', 'serlo-staging.dev']
 })
 
 export { Sentry }
