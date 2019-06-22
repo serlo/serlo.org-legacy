d01036f62e218614146c7123065514ba6fa81145
Jonas Keinholz <git@inyono.dev>
2019-06-22 15:11:31 +0200

    chore: yarn license

--- packages/public/server/__tests__/mails.ts
+++ packages/public/server/__tests__/mails.ts
@@ -1,13 +1,13 @@
-/*
- * This file is part of Athene2.
+/**
+ * This file is part of Serlo.org.
  *
- * Copyright (c) 2013-2018 Serlo Education e.V.
+ * Copyright (c) 2013-2019 Serlo Education e.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS
@@ -15,9 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @copyright Copyright (c) 2013-2018 Serlo Education e.V.
+ * @copyright Copyright (c) 2013-2019 Serlo Education e.V.
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
- * @link      https://github.com/serlo-org/athene2 for the canonical source repository
+ * @link      https://github.com/serlo-org/serlo.org for the canonical source repository
  */
 import axios from 'axios'
 import { getDocument, queries } from 'pptr-testing-library'
