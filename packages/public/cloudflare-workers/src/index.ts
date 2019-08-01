31db88347542d9a38678ff70036de18646e020ae
Jonas Keinholz <git@inyono.dev>
2019-08-01 03:32:29 +0200

    feat(cloudflare-workers): 100% kubernetes 🎉

--- packages/public/cloudflare-workers/src/index.ts
+++ packages/public/cloudflare-workers/src/index.ts
@@ -91,9 +91,9 @@ async function serloOrgProxy(request: Request) {
    * Experiment config
    */
   /** Change cookie name when changing probability so that a new cookie is created */
-  const cookieName = 'proxy-9'
+  const cookieName = 'proxy-10'
   /** Probablity (0 <= p <= 1) that the legacy backend is chosen */
-  const legacyProbability = 0.1
+  const legacyProbability = 0
 
   enum Backend {
     legacy = 'serlo.education',
