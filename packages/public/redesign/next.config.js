f81f11f2e5caa3f6d6789a4f72a48e5f3126b855
Jonas Keinholz <git@inyono.dev>
2019-08-29 10:48:46 +0200

    redesign: support multiroots

--- packages/public/redesign/next.config.js
+++ packages/public/redesign/next.config.js
@@ -1,15 +1,32 @@
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
+const webpack = require('webpack')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
-//
 
 module.exports = withCSS(
   withImages(
     withBundleAnalyzer({
       assetPrefix: 'http://localhost:3000',
       webpack: config => {
+        // for multiroot support we need to overwrite one specific file
+        // doing it with webpack
+        config.plugins.push(
+          new webpack.NormalModuleReplacementPlugin(
+            /(.*)node_modules(\\|\/)next(\\|\/)dist(\\|\/)client(\\|\/)index.js/,
+            function(resource) {
+              resource.resource = resource.resource.replace(
+                'node_modules\\next\\dist\\client\\index.js',
+                'patch\\next\\dist\\client\\index.js'
+              )
+              resource.resource = resource.resource.replace(
+                'node_modules/next/dist/client/index.js',
+                'patch/next/dist/client/index.js'
+              )
+            }
+          )
+        )
         return config
       }
     })
--- packages/public/redesign/pages/_document.js
+++ packages/public/redesign/pages/_document.js
@@ -33,8 +33,8 @@ export default class MyDocument extends Document {
       return (
         <>
           <MyScripts />
-          <Main />
-          <NextScript />
+          <MyMain />
+          <MyNextScript />
         </>
       )
     } else {
@@ -62,4 +62,113 @@ class MyScripts extends Head {
       </>
     )
   }
+}
+
+class MyMain extends Main {
+  render() {
+    const { inAmpMode, html } = this.context._documentProps
+    if (inAmpMode) return '__NEXT_AMP_RENDER_TARGET__'
+    // supporting multiroot in production, append page to id of react root
+    return (
+      <div
+        id={
+          '__next' + process.env.NODE_ENV === 'production'
+            ? this.context._documentProps.__NEXT_DATA__.page
+            : ''
+        }
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    )
+  }
+}
+
+class MyNextScript extends NextScript {
+  render() {
+    const {
+      staticMarkup,
+      assetPrefix,
+      inAmpMode,
+      devFiles,
+      files,
+      __NEXT_DATA__
+    } = this.context._documentProps
+
+    const { page, buildId } = __NEXT_DATA__
+
+    if (
+      !inAmpMode &&
+      process.env.NODE_ENV === 'production' &&
+      !staticMarkup &&
+      (!devFiles || devFiles.length === 0) &&
+      page !== '/_error' &&
+      this.getDynamicChunks().length === 0 &&
+      !(this.props.crossOrigin || process.crossOrigin) &&
+      !this.props.nonce &&
+      page !== '/' &&
+      buildId
+    ) {
+      // we are in production, adding page to data-tag
+      // making sure resources don't get loaded twice
+      return (
+        <>
+          <script
+            id={'__NEXT_DATA__' + page}
+            type="application/json"
+            dangerouslySetInnerHTML={{
+              __html: NextScript.getInlineScriptSource(
+                this.context._documentProps
+              )
+            }}
+          />
+          <script
+            async
+            key={page}
+            src={assetPrefix + `/_next/static/${buildId}/pages${page}.js`}
+          />
+          <script
+            key={'mulitroots'}
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (!window.NEXT_ROOTS) window.NEXT_ROOTS = []
+                window.NEXT_ROOTS.push("${page}")
+            `
+            }}
+          />
+          {createDedupScriptTag(
+            assetPrefix + `/_next/static/${buildId}/pages/_app.js`
+          )}
+          {files &&
+          files.length > 0 &&
+          files.map(file => {
+            if (!/\.js$/.exec(file)) {
+              return null
+            }
+            return createDedupScriptTag(`${assetPrefix}/_next/${file}`, file)
+          })}
+        </>
+      )
+    }
+
+    return NextScript.prototype.render.call(this)
+  }
+}
+
+function createDedupScriptTag(path, key) {
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          if (!window.NEXTJS_SCRIPT_CACHE) window.NEXTJS_SCRIPT_CACHE = {}
+          var cache = window.NEXTJS_SCRIPT_CACHE
+          if (!cache["${key}"]) {
+            var script = document.createElement("script");
+            script.src ="${path}";
+            document.head.appendChild(script);
+            cache["${key}"] = true
+          }
+        `
+      }}
+      key={key}
+    />
+  )
 }
