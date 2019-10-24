6589ee6c31cb773d3d810ff0c9a9b31763b8bcd4
Benjamin Knorr <knorr.b@gmx.de>
2019-10-24 17:43:14 +0200

    side-by-side view for changes in overlay

---
+++ packages/public/client/src/main/modules/diff/diff.tsx
@@ -0,0 +1,168 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import { convert, isEdtr } from '@serlo/legacy-editor-to-editor'
+import { cleanEdtrState } from '@serlo/edtr-io'
+import styled from 'styled-components'
+import $ from 'jquery'
+
+//@ts-ignore
+import t from '../../../modules/translator'
+
+export function initDiff() {
+  const $elements = $('.revision-compare')
+  if (!$elements.length) return
+
+  import('./helper').then(({ createPatch, Diff2Html }) => {
+    $elements.each((_index, el) => {
+      const title = $(el).data('field-name')
+      const $old = $('.revision-compare-old', el)
+      const $new = $('.revision-compare-new', el)
+
+      const diff = createPatch(title, ...prettify($old.text(), $new.text()))
+      const html = Diff2Html.getPrettyHtml(diff, {
+        inputFormat: 'diff',
+        showFiles: false,
+        matching: 'lines',
+        maxLineSizeInBlockForComparison: 1000,
+        rawTemplates: {
+          'line-by-line-file-diff': `
+<div class="d2h-file-diff">
+    <div class="d2h-code-wrapper">
+        <table class="d2h-diff-table">
+            <tbody class="d2h-diff-tbody">
+            {{{diffs}}}
+            </tbody>
+        </table>
+    </div>
+</div>
+`,
+          'generic-empty-diff': `
+<tr>
+    <td class="{{diffParser.LINE_TYPE.INFO}}">
+        <div class="{{contentClass}} {{diffParser.LINE_TYPE.INFO}}">
+            Keine Änderungen
+        </div>
+    </td>
+</tr>`
+        }
+      })
+
+      const sideBySideHtml = Diff2Html.getPrettyHtml(diff, {
+        inputFormat: 'diff',
+        showFiles: false,
+        matching: 'lines',
+        maxLineSizeInBlockForComparison: 1000,
+        outputFormat: 'side-by-side'
+      })
+      const modal = $('<div>').attr('id', `side-by-side-${title}`)
+      $(el).append(modal)
+      render(<SideBySide diffHtml={sideBySideHtml} />, modal.get(0))
+      $(el).append(html)
+    })
+  })
+}
+
+const Wrapper = styled.div({
+  textAlign: 'right'
+})
+const Modal = styled.div({
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  backgroundColor: 'rgba(100,100,100,0.6)',
+  zIndex: 1000,
+  width: '100%',
+  height: '100%'
+})
+const ModalInner = styled.div({
+  backgroundColor: '#fff',
+  position: 'relative',
+  margin: '5%',
+  padding: '20px',
+  width: '90%',
+  height: '90%',
+  overflow: 'scroll'
+})
+function SideBySide(props: { diffHtml: string }) {
+  const [open, setOpen] = React.useState(false)
+
+  const ref = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    if (!ref.current) return
+
+    $(ref.current)
+      .find('.d2h-file-side-diff')
+      .on('scroll', function() {
+        const $this = $(this)
+        const scrollPosition = $this.scrollLeft()
+        if (scrollPosition) {
+          $this
+            .closest('.d2h-file-wrapper')
+            .find('.d2h-file-side-diff')
+            .scrollLeft(scrollPosition)
+        }
+      })
+  }, [open])
+  return (
+    <Wrapper>
+      {open ? (
+        <Modal>
+          <ModalInner>
+            <a
+              onClick={() => {
+                setOpen(false)
+              }}
+            >
+              Close
+            </a>
+            <div
+              ref={ref}
+              dangerouslySetInnerHTML={{ __html: props.diffHtml }}
+            />
+            <a
+              onClick={() => {
+                setOpen(false)
+              }}
+            >
+              Close
+            </a>
+          </ModalInner>
+        </Modal>
+      ) : (
+        <a
+          onClick={() => {
+            setOpen(true)
+          }}
+        >
+          {t('Show changes side by side')}
+        </a>
+      )}
+    </Wrapper>
+  )
+}
+
+function prettify(
+  oldStringified: string,
+  newStringified: string
+): [string, string] {
+  try {
+    const parsed = JSON.parse(oldStringified.trim())
+    const parsedNew = JSON.parse(newStringified.trim())
+    if (isEdtr(parsedNew)) {
+      const oldEdtr = isEdtr(parsed) ? parsed : convert(parsed)
+      return [
+        JSON.stringify(cleanEdtrState(oldEdtr), null, ' '),
+        JSON.stringify(cleanEdtrState(parsedNew), null, ' ')
+      ]
+    }
+
+    return [
+      JSON.stringify(parsed, null, ' '),
+      JSON.stringify(parsedNew, null, ' ')
+    ]
+  } catch (e) {
+    // was no json.
+  }
+
+  return [oldStringified.trim(), newStringified.trim()]
+}
