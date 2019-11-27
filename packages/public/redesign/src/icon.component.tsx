aedd57fe885d2d49ddf7ce9df6796ac468bf488d
elpoto <1258870+elbotho@users.noreply.github.com>
2019-11-27 16:34:19 +0100

    fix build, run prettier in icon.component

--- packages/public/redesign/src/icon.component.tsx
+++ packages/public/redesign/src/icon.component.tsx
@@ -3,18 +3,107 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition, SizeProp } from '@fortawesome/fontawesome-svg-core'
 // import { Props as FontAwesomeProps } from '@fortawesome/react-fontawesome/index.d'
 
-import { faInfoCircle, faUserEdit, faUserPlus, faArrowCircleLeft,faArrowCircleRight,faArrowLeft,faArrowRight,faBars,faCaretDown,faCheck,faChevronCircleRight,faChevronUp,faComments,faCopy,faEnvelope,faFlag,faGraduationCap,faHandHoldingHeart,faHistory,faLink,faMapMarkerAlt,faNewspaper,faPencilAlt,faQuestionCircle,faRedo,faReply,faSave,faSearch,faSpinner,faTimes,faTrash,faUndo,faUserCircle,faUserGraduate,faVolumeUp, faCircle, faVideo, faCog, faShareAlt, faCompass
+import {
+  faInfoCircle,
+  faUserEdit,
+  faUserPlus,
+  faArrowCircleLeft,
+  faArrowCircleRight,
+  faArrowLeft,
+  faArrowRight,
+  faBars,
+  faCaretDown,
+  faCheck,
+  faChevronCircleRight,
+  faChevronUp,
+  faComments,
+  faCopy,
+  faEnvelope,
+  faFlag,
+  faGraduationCap,
+  faHandHoldingHeart,
+  faHistory,
+  faLink,
+  faMapMarkerAlt,
+  faNewspaper,
+  faPencilAlt,
+  faQuestionCircle,
+  faRedo,
+  faReply,
+  faSave,
+  faSearch,
+  faSpinner,
+  faTimes,
+  faTrash,
+  faUndo,
+  faUserCircle,
+  faUserGraduate,
+  faVolumeUp,
+  faCircle,
+  faVideo,
+  faCog,
+  faShareAlt,
+  faCompass
 } from '@fortawesome/free-solid-svg-icons'
 
-import { faTwitterSquare, faFacebookSquare, faGoogle, faGithubSquare, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons'
+import {
+  faTwitterSquare,
+  faFacebookSquare,
+  faGoogle,
+  faGithubSquare,
+  faWhatsappSquare
+} from '@fortawesome/free-brands-svg-icons'
 
 interface iconObject {
   [key: string]: IconDefinition
 }
 
-const icons: iconObject  = {
-  faInfoCircle: faInfoCircle, faUserEdit: faUserEdit, faUserPlus: faUserPlus, faArrowCircleLeft: faArrowCircleLeft,faArrowCircleRight: faArrowCircleRight,faArrowLeft: faArrowLeft,faArrowRight: faArrowRight,faBars: faBars,faCaretDown: faCaretDown,faCheck: faCheck,faChevronCircleRight: faChevronCircleRight,faChevronUp: faChevronUp,faComments: faComments,faCopy: faCopy,faEnvelope: faEnvelope,faFlag: faFlag,faGraduationCap: faGraduationCap,faHandHoldingHeart: faHandHoldingHeart,faHistory: faHistory,faLink: faLink,faMapMarkerAlt: faMapMarkerAlt,faNewspaper: faNewspaper,faPencilAlt: faPencilAlt,faQuestionCircle: faQuestionCircle,faRedo: faRedo,faReply: faReply,faSave: faSave,faSearch: faSearch,faSpinner: faSpinner,faTimes: faTimes,faTrash: faTrash,faUndo: faUndo,faUserCircle: faUserCircle,faUserGraduate: faUserGraduate,faVolumeUp: faVolumeUp,
-  faTwitterSquare: faTwitterSquare, faFacebookSquare: faFacebookSquare, faCircle: faCircle, faVideo: faVideo, faCog: faCog, faShareAlt: faShareAlt, faGoogle: faGoogle, faWhatsappSquare: faWhatsappSquare, faCompass: faCompass, faGithubSquare: faGithubSquare
+const icons: iconObject = {
+  faInfoCircle: faInfoCircle,
+  faUserEdit: faUserEdit,
+  faUserPlus: faUserPlus,
+  faArrowCircleLeft: faArrowCircleLeft,
+  faArrowCircleRight: faArrowCircleRight,
+  faArrowLeft: faArrowLeft,
+  faArrowRight: faArrowRight,
+  faBars: faBars,
+  faCaretDown: faCaretDown,
+  faCheck: faCheck,
+  faChevronCircleRight: faChevronCircleRight,
+  faChevronUp: faChevronUp,
+  faComments: faComments,
+  faCopy: faCopy,
+  faEnvelope: faEnvelope,
+  faFlag: faFlag,
+  faGraduationCap: faGraduationCap,
+  faHandHoldingHeart: faHandHoldingHeart,
+  faHistory: faHistory,
+  faLink: faLink,
+  faMapMarkerAlt: faMapMarkerAlt,
+  faNewspaper: faNewspaper,
+  faPencilAlt: faPencilAlt,
+  faQuestionCircle: faQuestionCircle,
+  faRedo: faRedo,
+  faReply: faReply,
+  faSave: faSave,
+  faSearch: faSearch,
+  faSpinner: faSpinner,
+  faTimes: faTimes,
+  faTrash: faTrash,
+  faUndo: faUndo,
+  faUserCircle: faUserCircle,
+  faUserGraduate: faUserGraduate,
+  faVolumeUp: faVolumeUp,
+  faTwitterSquare: faTwitterSquare,
+  faFacebookSquare: faFacebookSquare,
+  faCircle: faCircle,
+  faVideo: faVideo,
+  faCog: faCog,
+  faShareAlt: faShareAlt,
+  faGoogle: faGoogle,
+  faWhatsappSquare: faWhatsappSquare,
+  faCompass: faCompass,
+  faGithubSquare: faGithubSquare
 }
 
 export type AllowedIcons = keyof (typeof icons)
