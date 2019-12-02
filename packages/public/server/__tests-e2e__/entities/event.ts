42dc41df82be114e3e397dd09e33dd4af235aa3d
Stephan Kulla <git.mail@kulla.me>
2019-12-03 00:38:29 +0100

    server: Remove content type for events from title (#89)

---
+++ packages/public/server/__tests-e2e__/entities/event.ts
@@ -0,0 +1,55 @@
+/**
+ * This file is part of Serlo.org.
+ *
+ * Copyright (c) 2013-2019 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2013-2019 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/serlo.org for the canonical source repository
+ */
+import * as R from 'ramda'
+import { Browser, launch, Page } from 'puppeteer'
+
+describe('events', () => {
+  let browser: Browser
+  let page: Page
+
+  const eventUrl = 'http://de.serlo.localhost:4567/35554'
+
+  beforeAll(async () => {
+    browser = await launch()
+  })
+
+  beforeEach(async () => {
+    page = await browser.newPage()
+  })
+
+  afterEach(async () => {
+    await page.close()
+  })
+
+  afterAll(async () => {
+    await browser.close()
+  })
+
+  test('view page of an event', async () => {
+    await page.goto(eventUrl)
+
+    const title = await page.evaluate(() => {
+      return document.querySelector<HTMLElement>('h1#title')!.innerText
+    })
+    expect(title).toBe('Beispielveranstaltung')
+  })
+})
