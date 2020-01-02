f1a67c0fb4f9f35255b9567bcc7dd5f68a4767a0
Jonas Keinholz <git@inyono.dev>
2020-01-02 13:28:48 +0100

    frontend: possibly convert legacy content

--- packages/public/frontend/pages/__content.tsx
+++ packages/public/frontend/pages/__content.tsx
@@ -7,18 +7,25 @@ import { Renderer } from '../src/edtr-io'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { RendererProps } from '@edtr-io/renderer'
+import { convert, isEdtr } from '../src/legacy/legacy-editor-to-editor'
+import {
+  Edtr,
+  Legacy,
+  Splish
+} from '../src/legacy/legacy-editor-to-editor/splishToEdtr/types'
 // Prevent fontawesome from dynamically adding its css since we did it manually above
 config.autoAddCss = false
 
-export default function Index(props) {
-  // TODO: handle invalid states somewhere (e.g. numbers, super annoying)
-  // console.log(input)
-  const state: RendererProps['state'] = JSON.parse(
-    props.input.trim().replace(/&quot;/g, '"')
-  )
-  // TODO: handle convert?
-  // const state = isEdtr(data) ? data : convert(data)
-
+export default function Index({ input }) {
+  if (input === undefined) return null
+  if (input === '') return null
+  let data: Legacy | Splish | Edtr
+  try {
+    data = JSON.parse(input.trim().replace(/&quot;/g, '"'))
+  } catch (e) {
+    return null
+  }
+  const state = isEdtr(data) ? data : convert(data)
   return <Renderer state={state} />
 }
 Index.getInitialProps = async ({ req, res }) => {
