b0da345e76171245d39daddef5d93f7733c3007a
Stephan Kulla <git.mail@kulla.me>
2020-01-17 11:19:38 +0100

    e2e-testing: Remove unuecessary setup
    The setup is already provided by the preset of jest-puppeteer.

--- jest.setup-e2e.ts
+++ jest.setup-e2e.ts
@@ -19,22 +19,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/serlo.org for the canonical source repository
  */
-import { Browser, launch, Page } from 'puppeteer'
-
 setTimeout(60)
-
-let browser: Browser
-let page: Page
-
-beforeAll(async () => {
-  browser = await launch()
-  page = await browser.newPage()
-})
-
-afterAll(async () => {
-  await page.close()
-  await browser.close()
-})
 
 function setTimeout(seconds: number) {
   jest.setTimeout(seconds * 1000)
