8e0bbbb1d474ee4aa1965873862a325217f5c397
Stephan Kulla <git.mail@kulla.me>
2020-03-04 14:37:43 +0100

    donation-profiles: Add icons next to user links

--- packages/public/client/src/feature-prototypes/donation-profiles.ts
+++ packages/public/client/src/feature-prototypes/donation-profiles.ts
@@ -1,5 +1,38 @@
+const authors = ['26175']
+const reviewers = ['26175']
+const donors = ['26175']
+
 export function initDonationProfile(): void {
-  $(() => {
-    alert(42)
+  initIconsNextToUserLinks()
+}
+
+function initIconsNextToUserLinks(): void {
+  $('a').each((_, a) => {
+    const href = a.getAttribute('href')
+    const match = href === null ? null : href.match(/\/user\/profile\/(\d+)$/)
+
+    if (match) {
+      const userId = match[1]
+
+      if (donors.includes(userId)) {
+        addIconToUserLink(a, 'heart', 'color: #337ab7;')
+      }
+
+      if (reviewers.includes(userId)) {
+        addIconToUserLink(a, 'search')
+      }
+
+      if (authors.includes(userId)) {
+        addIconToUserLink(a, 'pencil')
+      }
+    }
   })
+}
+
+function addIconToUserLink(
+  a: HTMLElement,
+  icon: string,
+  style = 'color: black;'
+): void {
+  $(a).append(` <span class="fa fa-${icon}" style="${style}"></span>`)
 }
