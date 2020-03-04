65a0e533d0551ff1baafe427f58cd308a8ea21fa
Stephan Kulla <git.mail@kulla.me>
2020-03-04 15:20:00 +0100

    donation-profile: Add icons in profile header

--- packages/public/client/src/feature-prototypes/donation-profiles.ts
+++ packages/public/client/src/feature-prototypes/donation-profiles.ts
@@ -1,9 +1,15 @@
 const authors = ['26175']
 const reviewers = ['26175']
 const donors = ['26175']
+const icons = {
+  author: icon('pencil'),
+  reviewer: icon('search'),
+  donor: icon('heart', 'color: #337ab7;')
+}
 
 export function initDonationProfile(): void {
   initIconsNextToUserLinks()
+  initIconsNextToUserProfileHeader()
 }
 
 function initIconsNextToUserLinks(): void {
@@ -15,24 +21,50 @@ function initIconsNextToUserLinks(): void {
       const userId = match[1]
 
       if (donors.includes(userId)) {
-        addIconToUserLink(a, 'heart', 'color: #337ab7;')
+        $(a).append(icons.donor)
       }
 
       if (reviewers.includes(userId)) {
-        addIconToUserLink(a, 'search')
+        $(a).append(icons.reviewer)
       }
 
       if (authors.includes(userId)) {
-        addIconToUserLink(a, 'pencil')
+        $(a).append(icons.author)
       }
     }
   })
 }
 
-function addIconToUserLink(
-  a: HTMLElement,
-  icon: string,
-  style = 'color: black;'
-): void {
-  $(a).append(` <span class="fa fa-${icon}" style="${style}"></span>`)
+function initIconsNextToUserProfileHeader() {
+  if (
+    location.pathname.startsWith('/user/profile/') ||
+    location.pathname.startsWith('/user/me')
+  ) {
+    const links = Array.prototype.slice.call(document.querySelectorAll('a'))
+    const userId = links
+      .map(link => {
+        const href = link.getAttribute('href')
+        const match =
+          href === null ? null : href.match(/\/event\/history\/user\/(\d+)$/)
+
+        return match === null ? null : match[1]
+      })
+      .find(element => element !== null)
+
+    if (donors.includes(userId)) {
+      $('h1 > small').before(icons.donor)
+    }
+
+    if (reviewers.includes(userId)) {
+      $('h1 > small').before(icons.reviewer)
+    }
+
+    if (authors.includes(userId)) {
+      $('h1 > small').before(icons.author)
+    }
+  }
+}
+
+function icon(faType: string, style = 'color: black;'): string {
+  return ` <span class="fa fa-${faType}" style="${style}"></span> `
 }
