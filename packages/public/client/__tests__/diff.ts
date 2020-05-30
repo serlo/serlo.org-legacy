502aa621bdc4318965365b32785ed8d5bd39e5e2
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2020-05-30 23:25:17 +0200

    diff: serialize spoiler plugin

--- packages/public/client/__tests__/diff.ts
+++ packages/public/client/__tests__/diff.ts
@@ -1,9 +1,18 @@
 import { serialize } from '../src/main/modules/diff/serialize.ts'
 import * as diff_example from './diff_example.json'
+import * as diff_spoiler from './diff_spoiler.json'
 
 test("Does something", () => {
   console.log("Hello, World!");
   let serialized = serialize(diff_example);
+  console.log(serialized);
+  let bla = "a";
+  expect(bla).toEqual('a');
+})
+
+test("Does something else", () => {
+  console.log("Hello, Spoiler!");
+  let serialized = serialize(diff_spoiler);
   console.log(serialized);
   let bla = "a";
   expect(bla).toEqual('a');
--- packages/public/client/src/main/modules/diff/serialize.ts
+++ packages/public/client/src/main/modules/diff/serialize.ts
@@ -3,5 +3,17 @@ import { safeDump } from 'js-yaml'
 export function serialize(
   state: Object,
 ): string {
-  return safeDump(state)  
+  return safeDump(dumb(state))
+}
+
+function dumb(
+  state: Object,
+): Object {
+  if (state.plugin == "spoiler") {
+    let spoiler_header = "spoiler(" + state.state.title + ")"
+    let spoiler = Object()
+    spoiler[spoiler_header] = dumb(state.state.content)
+    return spoiler
+  }
+  return state
 }
