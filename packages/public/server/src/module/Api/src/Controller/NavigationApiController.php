e4dede57d1b2c9a97f481815f075b770dfd18ec9
Jonas Keinholz <git@inyono.dev>
2020-07-06 02:02:30 +0200

    yarn format

--- packages/public/server/src/module/Api/src/Controller/NavigationApiController.php
+++ packages/public/server/src/module/Api/src/Controller/NavigationApiController.php
@@ -47,14 +47,19 @@ class NavigationApiController extends AbstractActionController
 
     public function indexAction()
     {
-        $instance = $this->getInstanceManager()->getInstanceFromRequest()->getSubdomain();
+        $instance = $this->getInstanceManager()
+            ->getInstanceFromRequest()
+            ->getSubdomain();
         try {
             $pages = $this->navigationService->getNavigation('default');
             $pages = $this->formatPages($pages);
         } catch (ContainerNotFoundException $e) {
             $pages = [];
         }
-        return new JsonModel(['data' => json_encode($pages), 'instance' => $instance]);
+        return new JsonModel([
+            'data' => json_encode($pages),
+            'instance' => $instance,
+        ]);
     }
 
     protected function formatPages(array $pages)
