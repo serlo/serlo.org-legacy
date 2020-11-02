31bcef8cd3590639e7ddf6df14dc30e7d18c2fa2
Jonas Keinholz <git@inyono.dev>
2020-11-02 02:36:09 +0100

    deploy

--- packages/public/server/src/config/autoload/global.php
+++ packages/public/server/src/config/autoload/global.php
@@ -36,7 +36,7 @@ return [
         [
             'assets_host' =>
                 'https://packages.serlo.org/serlo-org-static-assets@2/',
-            'bundle_host' => 'https://packages.serlo.org/serlo-org-client@15/',
+            'bundle_host' => 'https://packages.serlo.org/serlo-org-client@16/',
         ],
         $assets
     ),
