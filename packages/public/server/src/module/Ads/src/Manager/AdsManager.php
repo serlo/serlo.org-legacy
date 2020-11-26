f78e512976e120e010d250f5dd3b0f8ede134afb
Stephan Kulla <git.mail@kulla.me>
2020-11-26 15:33:16 +0100

    server: Remove old and redundant serlo header

--- packages/public/server/src/module/Ads/src/Manager/AdsManager.php
+++ packages/public/server/src/module/Ads/src/Manager/AdsManager.php
@@ -21,13 +21,6 @@
  * @link      https://github.com/serlo-org/serlo.org for the canonical source repository
  */
 
-/**
- * Athene2 - Advanced Learning Resources Manager
- *
- * @author      Jakob Pfab (jakob.pfab@serlo.org)
- * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
- * @link        https://github.com/serlo-org/athene2 for the canonical source repository
- */
 namespace Ads\Manager;
 
 use Ads\Entity\AdInterface;
--- packages/public/server/src/module/Entity/src/Controller/RepositoryController.php
+++ packages/public/server/src/module/Entity/src/Controller/RepositoryController.php
@@ -21,14 +21,6 @@
  * @link      https://github.com/serlo-org/serlo.org for the canonical source repository
  */
 
-/**
- * Athene2 - Advanced Learning Resources Manager
- *
- * @author      Aeneas Rekkas (aeneas.rekkas@serlo.org)
- * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
- * @link        https://github.com/serlo-org/athene2 for the canonical source repository
- */
-
 namespace Entity\Controller;
 
 use Entity\Entity\EntityInterface;
