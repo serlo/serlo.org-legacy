1da5b363411408d0ff1d6e6d38d4c741483f1b53
Jonas Keinholz <git@inyono.dev>
2021-05-12 15:41:56 +0200

    server: release v18.0.2

--- packages/public/server/src/module/Api/src/ApiManager.php
+++ packages/public/server/src/module/Api/src/ApiManager.php
@@ -213,7 +213,10 @@ class ApiManager
                 $data['metaDescription'] = $uuid->get('meta_description', '');
             } elseif ($data['__typename'] === 'CoursePageRevision') {
                 $data['title'] = $uuid->get('title', '');
-                $data['content'] = $uuid->get('content', '{"plugin":"rows","state":[{"plugin":"text"}]}');
+                $data['content'] = $uuid->get(
+                    'content',
+                    '{"plugin":"rows","state":[{"plugin":"text"}]}'
+                );
                 $data['changes'] = $uuid->get('changes', '');
             } elseif ($data['__typename'] === 'EventRevision') {
                 $data['title'] = $uuid->get('title', '');
@@ -235,7 +238,10 @@ class ApiManager
                 $data['changes'] = $uuid->get('changes', '');
             } elseif ($data['__typename'] === 'VideoRevision') {
                 $data['title'] = $uuid->get('title', '');
-                $data['content'] = $uuid->get('description', '{"plugin":"rows","state":[{"plugin":"text"}]}');
+                $data['content'] = $uuid->get(
+                    'description',
+                    '{"plugin":"rows","state":[{"plugin":"text"}]}'
+                );
                 $data['url'] = $uuid->get('content', '');
                 $data['changes'] = $uuid->get('changes', '');
             }
